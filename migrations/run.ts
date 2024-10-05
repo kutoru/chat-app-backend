57cc8bcbcbaf49ae85012f95bc62e10bb4d543b6
kutoru <eg_bls@mail.ru>
2024-10-05 04:13:19 +0300

    Fix database update

--- migrations/run.ts
+++ migrations/run.ts
@@ -1,47 +1,38 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { getTransactionConnection } from "../src/database";
+import {
+  TransactionConnection,
+  getTransactionConnection,
+} from "../src/database";
 import fs from "fs/promises";
 import { Result } from "typescript-result";
 
-async function executeSqlFile(fileName: string) {
+async function executeSqlFile(
+  conn: TransactionConnection,
+  fileName: string,
+): Promise<Result<void, Error>> {
   const file = (await fs.readFile(fileName)).toString();
-
-  const queries = file
-    .split(";")
-    .filter((v) => v.trim().length > 0)
-    .map((v) => {
-      return { query: v };
-    });
-
-  const connResult = await getTransactionConnection();
-  if (connResult.isError()) {
-    return connResult;
-  }
-
-  const conn = connResult.getOrThrow();
+  const queries = file.split(";").filter((v) => v.trim().length > 0);
   const promises = [];
 
   for (let i = 0; i < queries.length; i++) {
-    promises.push(conn.query(queries[i].query));
+    promises.push(conn.query(queries[i]));
   }
 
-  const results = await Promise.allSettled(promises);
+  const results = await Promise.all(promises);
 
   for (let i = 0; i < results.length; i++) {
-    if (results[i].status === "rejected") {
-      const errorResult = results[i] as PromiseRejectedResult;
+    if (results[i].isError()) {
+      const errorMessage = results[i].error!.message;
 
       return Result.error(
-        new Error(
-          "Could not execute one of the queries: " + errorResult.reason,
-        ),
+        new Error("Could not execute one of the queries: " + errorMessage),
       );
     }
   }
 
-  return await conn.commit();
+  return Result.ok();
 }
 
 (async () => {
@@ -63,22 +54,34 @@ async function executeSqlFile(fileName: string) {
     }
   }
 
-  if (down) {
-    const downRes = await executeSqlFile("./migrations/down.sql");
-    console.log("Down result:", downRes);
+  const connRes = await getTransactionConnection();
+  if (connRes.isError()) {
+    console.log(connRes);
+    process.exit(1);
+  }
 
+  const conn = connRes.getOrThrow();
+
+  if (down) {
+    const downRes = await executeSqlFile(conn, "./migrations/down.sql");
     if (downRes.isError()) {
+      console.log(downRes);
       process.exit(1);
     }
   }
 
   if (up) {
-    const upRes = await executeSqlFile("./migrations/up.sql");
-    console.log("Up result:", upRes);
-
+    const upRes = await executeSqlFile(conn, "./migrations/up.sql");
     if (upRes.isError()) {
+      console.log(upRes);
       process.exit(1);
     }
+  }
+
+  const commitRes = await conn.commit();
+  if (commitRes.isError()) {
+    console.log(commitRes);
+    process.exit(1);
   }
 
   console.log("DB udpate successfully ended");
